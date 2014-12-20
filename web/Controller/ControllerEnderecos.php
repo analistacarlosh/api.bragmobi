b3d26b8afaa0a3125d1d225b082e310a7169dd21
analistacarlosh <analista.carlosh@gmail.com>
2014-12-20 10:15:46 -0200

    Definido requests defaults de enderecos

--- web/Controller/ControllerEnderecos.php
+++ web/Controller/ControllerEnderecos.php
@@ -4,18 +4,34 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+$response = array('message' => null, 'more' => null, 'ruas' => null);
 $enderecos = $app['controllers_factory'];
-$enderecos->get("/", function(){
-    return new Response("Enderecos");
-});
+$offsetDefault = 1;
+$limitDefault = 20;
 
-$enderecos->get("/bairros-de-braganca", function(){
-    return new Response("Exibe todas os bairros-de-braganca");
-});
+$enderecos->get("/{offset}/{limit}/", function(Silex\Application $app,  Request $request, $offset, $limit){
 
-$enderecos->get("/ruas-de-braganca", function(){
-    return new Response("Exibe todas as ruas-de-braganca");
-});
+		$sql = "SELECT * FROM ruas_braganca LIMIT " . $offset . ", " . $limit;
+    $response['ruas'] = $app['dbs']['mysql_read']->fetchAll($sql);	
+    $response['message']	= 'sucess';
+
+    return $app->json($response, 200);
+})->assert('offset', '\d+')
+	->assert('limit', '\d+')
+  ->value('offset', $offsetDefault)
+  ->value('limit', $limitDefault);
+
+$enderecos->get("/bairros-de-braganca", function(Silex\Application $app, $offset, $limit){
+
+	$sql = "SELECT * FROM bairros_braganca LIMIT " . $offset . ", " . $limit;
+  $response['bairros'] = $app['dbs']['mysql_read']->fetchAll($sql);	
+  $response['message']	= 'sucess';
+    
+  return $app->json($response, 200);
+})->assert('offset', '\d+')
+->assert('limit', '\d+')
+->value('offeset', $offsetDefault)
+->value('limit', $limitDefault);
 
 return $enderecos;
 
